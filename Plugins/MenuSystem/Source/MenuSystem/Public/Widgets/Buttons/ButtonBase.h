f3170c38598133b651f5ccb1597f32c03a36e393
Ali <elsaleh78@gmail.com>
2019-07-05 16:57:54 +1000

    Implement menu base

--- Plugins/MenuSystem/Source/MenuSystem/Public/Widgets/Buttons/ButtonBase.h
+++ Plugins/MenuSystem/Source/MenuSystem/Public/Widgets/Buttons/ButtonBase.h
@@ -2,9 +2,25 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "ButtonBase.generated.h"
+
+UENUM()
+enum EButtonType
+{
+	BTN_NEW_GAME	UMETA(DisplayName = "New game"),
+	BTN_CONTINUE	UMETA(DisplayName = "Continue"),
+	BTN_OPTIONS		UMETA(DisplayName = "Options"),
+	BTN_CREDITS		UMETA(DisplayName = "Credits"),
+	BTN_VIDEO		UMETA(DisplayName = "Video"),
+	BTN_AUDIO		UMETA(DisplayName = "Audio"),
+	BTN_CONTROLS	UMETA(DisplayName = "Controls"),
+	BTN_RESET_INPUT	UMETA(DisplayName = "Reset input bindings"),
+	BTN_APPLY		UMETA(DisplayName = "Apply"),
+	BTN_BACK		UMETA(DisplayName = "Back"),
+	BTN_EXIT		UMETA(DisplayName = "Exit"),
+	BTN_NONE		UMETA(DisplayName = "None")
+};
 
 /**
  * 
--- Plugins/MenuSystem/Source/MenuSystem/Public/Widgets/Menus/MenuBase.h
+++ Plugins/MenuSystem/Source/MenuSystem/Public/Widgets/Menus/MenuBase.h
@@ -1,10 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Ali El Saleh 2019
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Buttons/ButtonBase.h"
 #include "MenuBase.generated.h"
+
+UENUM()
+enum EMenuType
+{
+	MAIN,
+	OPTIONS,
+	NEW_GAME,
+	CONTROLS,
+	VIDEO,
+	AUDIO,
+	CREDITS
+};
 
 /**
  * 
@@ -15,6 +27,52 @@ class MENUSYSTEM_API UMenuBase : public UUserWidget
 	GENERATED_BODY()
 	
 	
-	
+public:
+	virtual void FadeIn();
+	virtual void FadeOut();
+
+	virtual void Init();
+
+	UFUNCTION(BlueprintCallable)
+		virtual void Apply();
+
+	UFUNCTION(BlueprintCallable)
+		virtual void Forward(EButtonType Menu);
+
+	UFUNCTION(BlueprintCallable)
+		virtual void Back();
+
+	virtual void SetMenuTooltipText(const FText& Text);
+
+protected:
+	virtual void InitializeSettings();
+	virtual void InitializeButtons();
+	virtual void GoForward();
+	virtual void GoBack();
+
+	void OnAnimationStarted_Implementation(const UWidgetAnimation* Animation) override;
+	void OnAnimationFinished_Implementation(const UWidgetAnimation* Animation) override;
+
+	void AddSetting(class UMenuSetting* Setting);
+
+	UFUNCTION(BlueprintCallable, Category = "Video Menu")
+		virtual void StoreAllSettings(class UPanelWidget* ParentWidget);
+
+	UPROPERTY(BlueprintReadWrite, Category = "Animations")
+		UWidgetAnimation* Animation {};
+
+	UPROPERTY(BlueprintReadOnly, Category = "Menu Base")
+		FText MenuTooltipText;
+
+	EButtonType MenuSelected;
+
+	UPanelWidget* ParentBox;
+
+	class AMenuHUD* MenuHUD{};
+
+	TArray<class UMenuSetting*> MenuSettings;
+
+	FTimerHandle ForwardTimerHandle;
+	FTimerHandle BackTimerHandle;
 	
 };
