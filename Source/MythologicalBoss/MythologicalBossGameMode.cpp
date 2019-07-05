bd72bf6942c24fc769a84d1e77698aeb259f7078
Ali <elsaleh78@gmail.com>
2019-07-05 13:55:09 +1000

    Reorganize folder
    Remove starter content

--- Source/MythologicalBoss/MythologicalBossGameMode.cpp
+++ Source/MythologicalBoss/MythologicalBossGameMode.cpp
@@ -7,7 +7,7 @@
 AMythologicalBossGameMode::AMythologicalBossGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Characters/Player/ThirdPersonCharacter.ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
