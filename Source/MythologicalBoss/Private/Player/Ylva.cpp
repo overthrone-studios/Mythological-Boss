20c78dd637178530075cef81348966bcecbc8882
Ali El Saleh <elsaleh78@gmail.com>
2019-07-16 13:40:07 +1000

    Fix include error
    Remove comments

--- Source/MythologicalBoss/Private/Player/Ylva.cpp
+++ Source/MythologicalBoss/Private/Player/Ylva.cpp
@@ -1,31 +1,26 @@
 // Copyright Overthrone Studios 2019
 
-#include "Ylva.h"
+#include "Player/Ylva.h"
 
 
-// Sets default values
 AYlva::AYlva()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-// Called when the game starts or when spawned
 void AYlva::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
-void AYlva::Tick(float DeltaTime)
+void AYlva::Tick(const float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AYlva::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
--- Source/MythologicalBoss/Public/Player/Ylva.h
+++ Source/MythologicalBoss/Public/Player/Ylva.h
@@ -2,30 +2,23 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Ylva.generated.h"
 
 UCLASS()
-class MYTHOLOGICALBOSS_API AYlva : public ACharacter
+class MYTHOLOGICALBOSS_API AYlva final : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AYlva();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	void BeginPlay() override;
+	void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
 	
 };
