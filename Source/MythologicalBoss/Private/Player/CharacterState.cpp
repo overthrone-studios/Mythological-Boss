f4c9933352b725a93ae475898edca9ae3dd3d4e7
Ali El Saleh <elsaleh78@gmail.com>
2019-07-16 14:45:39 +1000

    Add Character state interface

---
+++ Source/MythologicalBoss/Private/Player/CharacterState.cpp
@@ -0,0 +1,6 @@
+// Copyright Overthrone Studios 2019
+
+#include "Player/CharacterState.h"
+
+
+// Add default functionality here for any ICharacterState functions that are not pure virtual.
---
+++ Source/MythologicalBoss/Public/Player/CharacterState.h
@@ -0,0 +1,26 @@
+// Copyright Overthrone Studios 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "CharacterState.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UCharacterState : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * A list of all the character states the player will implement
+ */
+class MYTHOLOGICALBOSS_API ICharacterState
+{
+	GENERATED_BODY()
+
+public:
+	
+	
+};
