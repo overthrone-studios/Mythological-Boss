bd92cf2efee1c5bf38395321ea0846f5ebecef1c
Ali El Saleh <elsaleh78@gmail.com>
2019-08-13 23:17:06 +1000

    Simplify code

--- Source/MythologicalBoss/Private/Boss/MordathAnimInstance.cpp
+++ Source/MythologicalBoss/Private/Boss/MordathAnimInstance.cpp
@@ -1,35 +1,12 @@
 // Copyright Overthrone Studios 2019
 
 #include "MordathAnimInstance.h"
-#include "Mordath.h"
 
-void UMordathAnimInstance::NativeInitializeAnimation()
+void UMordathAnimInstance::LeaveAllStates()
 {
-	Super::NativeInitializeAnimation();
+	Super::LeaveAllStates();
 
-	// Cache the owning pawn for use in Tick
-	OwningPawn = TryGetPawnOwner();
-
-	// Cache the owning pawn's movement component for use in Tick
-	if (OwningPawn)
-	{
-		PawnMovementComponent = OwningPawn->GetMovementComponent();
-
-		// If it's our character cache it for later use
-		if (OwningPawn->IsA(AMordath::StaticClass()))
-			Mordath = Cast<AMordath>(OwningPawn);
-	}
-
-	GenericsMachineIndex = GetStateMachineIndex("Generics");
-}
-
-void UMordathAnimInstance::NativeUpdateAnimation(const float DeltaSeconds)
-{
-	Super::NativeUpdateAnimation(DeltaSeconds);
-
-	if (!OwningPawn || !PawnMovementComponent)
-		return;
-
-	// Update movement speed for use in the Idle/Run blendspace
-	MovementSpeed = Mordath->GetVelocity().Size();
+	bCanLaugh = false;
+	bIsBeaten = false;
+	bIsStunned = false;
 }
--- Source/MythologicalBoss/Public/Boss/MordathAnimInstance.h
+++ Source/MythologicalBoss/Public/Boss/MordathAnimInstance.h
@@ -3,66 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Animation/AnimInstance.h"
+#include "OverthroneAnimInstance.h"
 #include "MordathAnimInstance.generated.h"
 
 /**
  * The animation instance the boss character will use
  */
 UCLASS()
-class MYTHOLOGICALBOSS_API UMordathAnimInstance final : public UAnimInstance
+class MYTHOLOGICALBOSS_API UMordathAnimInstance final : public UOverthroneAnimInstance
 {
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(BlueprintReadOnly, Category = "Movement")
-		float MovementSpeed = 0.0f;
+	void LeaveAllStates() override;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bIsHit;
+		uint8 bIsStunned : 1;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bIsStunned;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bIsBeaten;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bIsDead;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bAcceptLightAttack;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bAcceptSecondLightAttack;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bAcceptThirdLightAttack;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bAcceptHeavyAttack;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bAcceptSecondHeavyAttack;
-	
-	UPROPERTY(BlueprintReadOnly, Category = "Combat")
-		bool bAcceptThirdHeavyAttack;
+		uint8 bIsBeaten : 1;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Misc")
-		bool bCanLaugh;
-
-	// Used to get a reference to the anim state machine, and to query time remaining
-	int32 GenericsMachineIndex;
-	
-protected:
-	void NativeInitializeAnimation() override;
-	void NativeUpdateAnimation(float DeltaSeconds) override;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Mordath Anim Instance")
-		APawn* OwningPawn{};
-
-	UPROPERTY(BlueprintReadOnly, Category = "Mordath Anim Instance")
-		UPawnMovementComponent* PawnMovementComponent{};
-
-	class AMordath* Mordath{};
+		uint8 bCanLaugh : 1;
 };
