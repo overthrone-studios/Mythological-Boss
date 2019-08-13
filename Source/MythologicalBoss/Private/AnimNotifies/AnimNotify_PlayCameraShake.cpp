9481d652b5ea4f7dd0e2a87b01cc3c23109b886b
Ali El Saleh <elsaleh78@gmail.com>
2019-08-13 13:48:04 +1000

    Log the animation's name for better debugging

--- Source/MythologicalBoss/Private/AnimNotifies/AnimNotify_PlayCameraShake.cpp
+++ Source/MythologicalBoss/Private/AnimNotifies/AnimNotify_PlayCameraShake.cpp
@@ -4,6 +4,7 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "GameFramework/Character.h"
+#include "Animation/AnimSequenceBase.h"
 #include "Log.h"
 
 void UAnimNotify_PlayCameraShake::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
@@ -18,12 +19,11 @@ void UAnimNotify_PlayCameraShake::Notify(USkeletalMeshComponent* MeshComp, UAnim
 		if (CameraShake)
 		{
 			const float Distance = FVector::Dist(Player->GetActorLocation(), MeshComp->GetOwner()->GetActorLocation());
-			//ULog::DebugMessage(INFO, FString::SanitizeFloat(Distance), true);
 			const float NewIntensity = FMath::GetMappedRangeValueClamped(FVector2D(1500.0f, 0.0f), FVector2D(0.0f, Intensity), Distance);
-			//ULog::DebugMessage(INFO, "Intensity:" + FString::SanitizeFloat(Distance), true);
+
 			PlayerController->ClientPlayCameraShake(CameraShake, NewIntensity);
 		}
 		else
-			ULog::DebugMessage(WARNING, "Camera shake not found.", true);
+			ULog::DebugMessage(WARNING, Animation->GetName() + ": Camera shake not found.", true);
 	}
 }
