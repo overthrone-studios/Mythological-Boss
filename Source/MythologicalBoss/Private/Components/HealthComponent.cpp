d4084cab711e62773f7cc92953bf53ba6f0b42a1
Ali El Saleh <elsaleh78@gmail.com>
2019-08-19 13:00:07 +1000

    Create a health component

---
+++ Source/MythologicalBoss/Private/Components/HealthComponent.cpp
@@ -0,0 +1,83 @@
+// Copyright Overthrone Studios 2019
+
+#include "Components/HealthComponent.h"
+#include "OverthroneCharacter.h"
+#include "GameFramework/Actor.h"
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	DefaultHealth = 100.0f;
+
+	InitHealth();
+}
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitHealth();
+
+	// Retrieve the owner of this component and bind our function to the OnTakeAnyDamage event
+	Owner = GetOwner();
+	Owner->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::TakeDamage);
+}
+
+#if WITH_EDITOR
+void UHealthComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	// If default health value has changed, up current health
+	if (PropertyChangedEvent.GetPropertyName() == "DefaultHealth")
+		Health = DefaultHealth;
+}
+#endif
+
+void UHealthComponent::InitHealth()
+{
+	Health = DefaultHealth;
+	PreviousHealth = DefaultHealth;
+	NewHealth = DefaultHealth;
+}
+
+void UHealthComponent::SetHealth(const float NewHealthAmount)
+{
+	PreviousHealth = Health;
+
+	Health = FMath::Clamp(NewHealthAmount, 0.0f, DefaultHealth);
+	NewHealth = Health;
+}
+
+void UHealthComponent::SetNewHealth(const float Value)
+{
+	NewHealth = Value;
+}
+
+void UHealthComponent::IncreaseHealth(const float Amount)
+{
+	PreviousHealth = Health;
+
+	Health = FMath::Clamp(Health + Amount, 0.0f, DefaultHealth);
+	NewHealth = Health;
+}
+
+void UHealthComponent::DecreaseHealth(const float Amount)
+{
+	PreviousHealth = Health;
+
+	Health = FMath::Clamp(Health - Amount, 0.0f, DefaultHealth);
+	NewHealth = Health;
+}
+
+void UHealthComponent::ResetHealth()
+{
+	InitHealth();
+}
+
+void UHealthComponent::TakeDamage(AActor* DamagedActor, const float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (Damage <= 0)
+		return;
+
+	Owner->TakeDamage(Damage, FDamageEvent(), InstigatedBy, DamageCauser);
+}
---
+++ Source/MythologicalBoss/Public/Components/HealthComponent.h
@@ -0,0 +1,108 @@
+// Copyright Overthrone Studios 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Health), meta=(BlueprintSpawnableComponent), HideCategories=("Tags", "Activation", "Cooking", "AssetUserData", "Collision"))
+class MYTHOLOGICALBOSS_API UHealthComponent final : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHealthComponent();
+
+	// Return the actor's default health value
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE float GetDefaultHealth() const { return DefaultHealth; }
+
+	// Return the actor's current health value
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE float GetCurrentHealth() const { return Health; }
+
+	// Return the actor's previous health value
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE float GetPreviousHealth() const { return PreviousHealth; }
+
+	// Return the actor's new health value
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE float GetNewHealth() const { return NewHealth; }
+
+	// Return the actor's new health value
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE float GetLowHealthThreshold() const { return LowHealthThreshold; }
+	
+	// Returns true if smooth bar is enabled
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE bool IsUsingSmoothBar() const { return bSmoothBar; }
+
+	// Returns true if the actor's health is less than or equal to the low health threshold
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE bool IsLowHealth() const { return Health <= DefaultHealth * LowHealthThreshold; }
+	
+	// Set a new current health value
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		void SetHealth(float NewHealthAmount);
+
+	// Set the new health value
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		void SetNewHealth(float Value);
+
+	// Add health by an amount
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		void IncreaseHealth(float Amount);
+
+	// Subtract health by an amount
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		void DecreaseHealth(float Amount);
+
+	// Resets health back to default
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		void ResetHealth();
+
+protected:
+	void BeginPlay() override;
+
+	#if WITH_EDITOR
+	void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	#endif
+
+	// Initialize the actor's health values
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		void InitHealth();
+
+	// Called when the actor has taken damage
+	UFUNCTION()
+		void TakeDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+	// The actor's current health
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Health", meta = (ClampMin = 0.0f))
+		float Health = 100.0f;
+
+	// The actor's default health
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Health", meta = (ClampMin = 0.0f))
+		float DefaultHealth = 100.0f;
+
+	// The actor's low health threshold as percentage
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Health", meta = (ClampMin = 0.0f, ClampMax = 1.0f))
+		float LowHealthThreshold = 0.25f;
+
+	// Should we use a smooth health bar?
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Health")
+		uint8 bSmoothBar : 1;
+
+	// The actor's previous health before being damaged
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+		float PreviousHealth;
+
+	// The actor's updated health when bSmoothBar is enabled
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+		float NewHealth;
+
+	// The actor that owns this component
+	UPROPERTY(BlueprintReadOnly)
+		AActor* Owner;
+};
