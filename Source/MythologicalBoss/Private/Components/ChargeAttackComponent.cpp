47c34e85133e0ffd011b2783bab97e74604aac8a
Ali El Saleh <elsaleh78@gmail.com>
2019-08-19 19:35:19 +1000

    Set default values

--- Source/MythologicalBoss/Private/Components/ChargeAttackComponent.cpp
+++ Source/MythologicalBoss/Private/Components/ChargeAttackComponent.cpp
@@ -6,12 +6,23 @@ UChargeAttackComponent::UChargeAttackComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 
-}
+	MaxCharge = 100.0f;
 
+	InitChargeAttack();
+}
 
 void UChargeAttackComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
+	Owner = GetOwner();
+
+	InitChargeAttack();
+}
+
+void UChargeAttackComponent::InitChargeAttack()
+{
+	Charge = MaxCharge;
+	PreviousCharge = MaxCharge;
+	NewCharge = MaxCharge;
 }
