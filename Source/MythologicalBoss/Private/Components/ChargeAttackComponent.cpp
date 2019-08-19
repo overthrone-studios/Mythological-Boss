d6331836d6b1bc7b1194a87aeeddd05ea7a244ec
Ali El Saleh <elsaleh78@gmail.com>
2019-08-19 20:28:56 +1000

    Add more helper functions

--- Source/MythologicalBoss/Private/Components/ChargeAttackComponent.cpp
+++ Source/MythologicalBoss/Private/Components/ChargeAttackComponent.cpp
@@ -1,6 +1,8 @@
 // Copyright Overthrone Studios 2019
 
 #include "Components/ChargeAttackComponent.h"
+#include "GameFramework/Actor.h"
+#include "TimerManager.h"
 
 UChargeAttackComponent::UChargeAttackComponent()
 {
@@ -8,6 +10,50 @@ UChargeAttackComponent::UChargeAttackComponent()
 
 	MaxCharge = 100.0f;
 
+	InitChargeAttack();
+}
+
+void UChargeAttackComponent::DelayChargeLoss()
+{
+	Owner->GetWorldTimerManager().SetTimer(ChargeLossTimerHandle, DelayBeforeChargeLoss, false);
+}
+
+bool UChargeAttackComponent::IsLosingCharge()
+{
+	return !Owner->GetWorldTimerManager().IsTimerActive(ChargeLossTimerHandle);
+}
+
+void UChargeAttackComponent::SetCharge(const float NewChargeAmount)
+{
+	PreviousCharge = Charge;
+
+	Charge = FMath::Clamp(NewChargeAmount, 0.0f, MaxCharge);
+	NewCharge = Charge;
+}
+
+void UChargeAttackComponent::SetSmoothedCharge(const float Value)
+{
+	NewCharge = Value;
+}
+
+void UChargeAttackComponent::IncreaseCharge(const float Amount)
+{
+	PreviousCharge = Charge;
+
+	Charge = FMath::Clamp(Charge + Amount, 0.0f, MaxCharge);
+	NewCharge = Charge;
+}
+
+void UChargeAttackComponent::DecreaseCharge(const float Amount)
+{
+	PreviousCharge = Charge;
+
+	Charge = FMath::Clamp(Charge - Amount, 0.0f, MaxCharge);
+	NewCharge = Charge;
+}
+
+void UChargeAttackComponent::ResetCharge()
+{
 	InitChargeAttack();
 }
 
@@ -22,7 +68,7 @@ void UChargeAttackComponent::BeginPlay()
 
 void UChargeAttackComponent::InitChargeAttack()
 {
-	Charge = MaxCharge;
-	PreviousCharge = MaxCharge;
-	NewCharge = MaxCharge;
+	Charge = 0.0f;
+	PreviousCharge = 0.0f;
+	NewCharge = 0.0f;
 }
--- Source/MythologicalBoss/Public/Components/ChargeAttackComponent.h
+++ Source/MythologicalBoss/Public/Components/ChargeAttackComponent.h
@@ -15,6 +15,86 @@ class MYTHOLOGICALBOSS_API UChargeAttackComponent final : public UActorComponent
 public:	
 	UChargeAttackComponent();
 
+	// Delay charge loss by the DelayBeforeChargeLoss property
+	UFUNCTION(BlueprintCallable, Category = "Charge Attack")
+		void DelayChargeLoss();
+
+	// Returns true if the charge loss timer is inactive
+	UFUNCTION(BlueprintCallable, Category = "Charge Attack")
+		bool IsLosingCharge();
+
+	// Return the actor's maximum hits
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE uint8 GetMaxHits() const { return MaxHitsForChargeReset; }
+
+	// Return the actor's default charge value
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE float GetMaxCharge() const { return MaxCharge; }
+
+	// Return the actor's current charge value
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE float GetCurrentCharge() const { return Charge; }
+
+	// Return the actor's previous charge value
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE float GetPreviousCharge() const { return PreviousCharge; }
+
+	// Return the actor's smoothed charge value
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE float GetSmoothedCharge() const { return NewCharge; }
+	
+	// Returns true if smooth bar is enabled
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE bool IsUsingSmoothBar() const { return bSmoothBar; }
+
+	// Returns true if our current charge value is >= to our maximum charge value
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE bool IsChargeFull() const { return Charge >= MaxCharge; }
+
+	// Returns true if our current charge value is <= 0
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE bool IsChargeEmpty() const { return Charge <= 0.0f; }
+
+	// Returns true if we can lose charge overtime
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE bool CanLoseChargeOvertime() const { return bLoseChargeOvertime; }
+
+	// Returns true if we want to reset charge after max hits
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE bool WantsResetAfterMaxHits() const { return bResetChargeAfterMaxHits; }
+
+	// Return the charge gain per hit value
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE float GetChargeGain() const { return ChargeGainPerHit; }
+
+	// Return the charge loss per hit value
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE float GetChargeLoss() const { return ChargeLossPerHit; }
+
+	// Return the charge loss rate
+	UFUNCTION(BlueprintPure, Category = "Charge Attack")
+		FORCEINLINE float GetChargeLossRate() const { return ChargeLossRate; }
+
+	// Set a new current charge value
+	UFUNCTION(BlueprintCallable, Category = "Charge Attack")
+		void SetCharge(float NewChargeAmount);
+
+	// Set the new charge value
+	UFUNCTION(BlueprintCallable, Category = "Charge Attack")
+		void SetSmoothedCharge(float Value);
+
+	// Add charge by an amount
+	UFUNCTION(BlueprintCallable, Category = "Charge Attack")
+		void IncreaseCharge(float Amount);
+
+	// Subtract charge by an amount
+	UFUNCTION(BlueprintCallable, Category = "Charge Attack")
+		void DecreaseCharge(float Amount);
+
+	// Resets charge back to default
+	UFUNCTION(BlueprintCallable, Category = "Charge Attack")
+		void ResetCharge();
+
 protected:
 	void BeginPlay() override;
 
@@ -73,4 +153,6 @@ protected:
 	// The actor that owns this component
 	UPROPERTY(BlueprintReadOnly)
 		AActor* Owner;
+
+	FTimerHandle ChargeLossTimerHandle;
 };
