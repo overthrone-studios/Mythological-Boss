443d0f78a04ef5192c3461c15438ebcacf9e470e
Ali El Saleh <elsaleh78@gmail.com>
2019-08-21 21:08:06 +1000

    Implement the combo system foundation

--- Source/MythologicalBoss/Private/Components/AttackComboComponent.cpp
+++ Source/MythologicalBoss/Private/Components/AttackComboComponent.cpp
@@ -1,24 +1,124 @@
 // Copyright Overthrone Studios 2019
 
 #include "AttackComboComponent.h"
-
+#include "GameFramework/Actor.h"
+#include "TimerManager.h"
+#include "Log.h"
 
 UAttackComboComponent::UAttackComboComponent()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 }
-
 
 void UAttackComboComponent::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	Owner = GetOwner();
 }
 
-
-void UAttackComboComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+uint8 UAttackComboComponent::AdvanceCombo(const EAttackType InAttackType)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	uint8 ComboIndex = 0;
+
+	switch (InAttackType)
+	{
+	case Light:
+		if (LightAttacks.Num() == 0)
+		{
+			ULog::Error("Could not advance the combo tree. There must be at least 1 attack in the Light Attacks list", true);
+			return TreeIndex;
+		}
+
+		ComboIndex = AdvanceCombo_Internal(Light);
+	break;
+
+	case Heavy:
+		if (HeavyAttacks.Num() == 0)
+		{
+			ULog::Error("Could not advance the combo tree. There must be at least 1 attack in the Heavy Attacks list", true);
+			return TreeIndex;
+		}
+
+		ComboIndex = AdvanceCombo_Internal(Heavy);
+
+	break;
+
+	case Special:
+		if (SpecialAttacks.Num() == 0)
+		{
+			ULog::Error("Could not advance the combo tree. There must be at least 1 attack in the Special Attacks list", true);
+			return TreeIndex;
+		}
+
+		ComboIndex = AdvanceCombo_Internal(Special);
+	break;
+	}
+
+	return ComboIndex;
 }
 
+uint8 UAttackComboComponent::AdvanceCombo_Internal(const enum EAttackType InAttackType)
+{
+	if (TreeIndex >= ComboTreeDepth)
+	{
+		if (bDebug)
+			ULog::Info("Reached the max tree depth, resetting...", true);
+
+		ResetCombo();
+
+		return TreeIndex;
+	}
+
+	// Start the combo reset timer
+	Owner->GetWorldTimerManager().SetTimer(ComboResetTimerHandle, this, &UAttackComboComponent::ResetCombo, ComboResetTime, false); 
+
+	TreeIndex++;
+
+	uint8 IndexToReturn = TreeIndex;
+
+	// Choose the attack
+	switch (InAttackType)
+	{
+	case Light:
+		LightAttackIndex++;
+		IndexToReturn = LightAttackIndex;
+
+		Combo.Add(Light);
+	break;
+
+	case Heavy:
+		HeavyAttackIndex++;
+		IndexToReturn = HeavyAttackIndex;
+
+		Combo.Add(Heavy);
+	break;
+
+	case Special:
+		SpecialAttackIndex++;
+		IndexToReturn = SpecialAttackIndex;
+
+		Combo.Add(Special);
+	break;
+	}
+
+	if (bDebug)
+		ULog::Number(TreeIndex, "Combo Index: ", true);
+
+	return IndexToReturn;
+}
+
+void UAttackComboComponent::ResetCombo()
+{
+	TreeIndex = 0;
+	LightAttackIndex = 0;
+	HeavyAttackIndex = 0;
+	SpecialAttackIndex = 0;
+
+	Combo.Empty();
+	Owner->GetWorldTimerManager().ClearTimer(ComboResetTimerHandle);
+
+	if (bDebug)
+		ULog::Success("Combo reset!", true);
+}
--- Source/MythologicalBoss/Public/Components/AttackComboComponent.h
+++ Source/MythologicalBoss/Public/Components/AttackComboComponent.h
@@ -6,8 +6,17 @@
 #include "Components/ActorComponent.h"
 #include "AttackComboComponent.generated.h"
 
+UENUM()
+enum EAttackType
+{
+	Light,
+	Heavy,
+	Special,
+	None
+};
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), HideCategories=("Tags", "Activation", "Cooking", "AssetUserData", "Collision"))
 class MYTHOLOGICALBOSS_API UAttackComboComponent final : public UActorComponent
 {
 	GENERATED_BODY()
@@ -15,8 +24,78 @@ class MYTHOLOGICALBOSS_API UAttackComboComponent final : public UActorComponent
 public:	
 	UAttackComboComponent();
 
+	// Advances to the next attack in the combo tree
+	UFUNCTION(BlueprintCallable, Category = "Attack Combo")
+		uint8 AdvanceCombo(enum EAttackType InAttackType);
+
+	// Returns the array of light attacks
+	UFUNCTION(BlueprintPure, Category = "Attack Combo")
+		FORCEINLINE TArray<class UAnimMontage*> GetLightAttacks() const { return LightAttacks; }
+
+	// Returns the array of heavy attacks
+	UFUNCTION(BlueprintPure, Category = "Attack Combo")
+		FORCEINLINE TArray<class UAnimMontage*> GetHeavyAttacks() const { return HeavyAttacks; }
+
+	// Returns the array of special attacks
+	UFUNCTION(BlueprintPure, Category = "Attack Combo")
+		FORCEINLINE TArray<class UAnimMontage*> GetSpecialAttacks() const { return SpecialAttacks; }
+
+	// Returns the current of light attack anim montage
+	UFUNCTION(BlueprintPure, Category = "Attack Combo")
+		FORCEINLINE class UAnimMontage* GetCurrentLightAttackAnim() const { return LightAttacks[LightAttackIndex-1]; }
+
+	// Returns the current of heavy attack anim montage
+	UFUNCTION(BlueprintPure, Category = "Attack Combo")
+		FORCEINLINE class UAnimMontage* GetCurrentHeavyAttackAnim() const { return HeavyAttacks[HeavyAttackIndex-1]; }
+
+	// Returns the current of special attack anim montage
+	UFUNCTION(BlueprintPure, Category = "Attack Combo")
+		FORCEINLINE class UAnimMontage* GetCurrentSpecialAttackAnim() const { return SpecialAttacks[SpecialAttackIndex-1]; }
+
 protected:
 	void BeginPlay() override;
-	void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	// Resets the combo tree
+	void ResetCombo();
+
+	// The list how light attacks we can use
+	UPROPERTY(EditInstanceOnly, Category = "Combo")
+		TArray<class UAnimMontage*> LightAttacks;
+
+	// The list how heavy attacks we can use
+	UPROPERTY(EditInstanceOnly, Category = "Combo")
+		TArray<class UAnimMontage*> HeavyAttacks;
+
+	// The list how special attacks we can use
+	UPROPERTY(EditInstanceOnly, Category = "Combo")
+		TArray<class UAnimMontage*> SpecialAttacks;
+
+	// How deep can the combo tree go?
+	UPROPERTY(EditInstanceOnly, Category = "Combo Tree", meta = (ClampMin = 1))
+		uint8 ComboTreeDepth = 1;
+
+	// How long (in seconds) should we wait until we reset the combo?
+	UPROPERTY(EditInstanceOnly, Category = "Combo Tree", meta = (ClampMin = 0.0f, ClampMax = 10.0f))
+		float ComboResetTime = 1.0f;
+
+	// Log combo tree status
+	UPROPERTY(EditInstanceOnly, Category = "Combo Tree")
+		uint8 bDebug : 1;
+
+private:
+	uint8 AdvanceCombo_Internal(enum EAttackType InAttackType);
+
+	// Tracks the attacks we've chained
+	TArray<TEnumAsByte<EAttackType>> Combo;
+
+	uint8 LightAttackIndex = 0;
+	uint8 HeavyAttackIndex = 0;
+	uint8 SpecialAttackIndex = 0;
+
+	uint8 TreeIndex = 0;
+
+	FTimerHandle ComboResetTimerHandle;
+
+	// The actor that owns this component
+	AActor* Owner;
 };
