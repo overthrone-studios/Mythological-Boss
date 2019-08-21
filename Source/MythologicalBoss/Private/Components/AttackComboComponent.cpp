c88653b06d5d38bf34d0abc7a5dbc0a19b4a4447
Ali El Saleh <elsaleh78@gmail.com>
2019-08-21 19:05:16 +1000

    Remove comments

--- Source/MythologicalBoss/Private/Components/AttackComboComponent.cpp
+++ Source/MythologicalBoss/Private/Components/AttackComboComponent.cpp
@@ -3,32 +3,22 @@
 #include "AttackComboComponent.h"
 
 
-// Sets default values for this component's properties
 UAttackComboComponent::UAttackComboComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
-// Called when the game starts
 void UAttackComboComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 	
 }
 
 
-// Called every frame
 void UAttackComboComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
--- Source/MythologicalBoss/Public/Components/AttackComboComponent.h
+++ Source/MythologicalBoss/Public/Components/AttackComboComponent.h
@@ -7,23 +7,16 @@
 #include "AttackComboComponent.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class MYTHOLOGICALBOSS_API UAttackComboComponent : public UActorComponent
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class MYTHOLOGICALBOSS_API UAttackComboComponent final : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UAttackComboComponent();
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+	void BeginPlay() override;
+	void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
-	
 };
