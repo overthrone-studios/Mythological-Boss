a6cd0128a2d03c24f93e1cd1ca3539ae15575c94
Ali El Saleh <elsaleh78@gmail.com>
2019-08-27 11:52:27 +1000

    Use EditAnywhere instead of EditInstanceOnly

--- Source/MythologicalBoss/Public/Components/FloatingComponent.h
+++ Source/MythologicalBoss/Public/Components/FloatingComponent.h
@@ -20,11 +20,11 @@ protected:
 	void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	// The amount of units to offset by on an axis
-	UPROPERTY(EditInstanceOnly, Category = "Floating Movement")
+	UPROPERTY(EditAnywhere, Category = "Floating Movement")
 		FVector Offset = FVector(0.0f, 0.0f, 50.0f);
 
 	// The movement speed while floating (1 = normal speed | >1 = fast | <1 = slow)
-	UPROPERTY(EditInstanceOnly, Category = "Floating Movement", meta = (ClampMin = 0.0f))
+	UPROPERTY(EditAnywhere, Category = "Floating Movement", meta = (ClampMin = 0.0f))
 		float Speed = 1;
 
 private:
