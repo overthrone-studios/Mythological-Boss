2a88c268a5665c6c85ed7461ead2531e8b185f32
Ali El Saleh <elsaleh78@gmail.com>
2019-08-31 13:21:20 +1000

    Add functions to toggle visibility

--- Source/MythologicalBoss/Private/Widgets/World/LockOn.cpp
+++ Source/MythologicalBoss/Private/Widgets/World/LockOn.cpp
@@ -1,13 +1,16 @@
 // Copyright Overthrone Studios 2019
 
 #include "LockOn.h"
+
 #include "Public/OverthroneGameInstance.h"
+
+#include "Components/WidgetComponent.h"
+
 #include "Engine/World.h"
 
 ALockOn::ALockOn()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
 }
 
 void ALockOn::BeginPlay()
@@ -15,6 +18,32 @@ void ALockOn::BeginPlay()
 	Super::BeginPlay();
 
 	class UOverthroneGameInstance* GameInstance = Cast<UOverthroneGameInstance>(GetWorld()->GetGameInstance());
-
 	GameInstance->LockOn = this;
+
+	// Get our lock on widget component
+	const auto Components = GetComponents();
+	for (auto Component : Components)
+	{
+		if (Component->IsA(UWidgetComponent::StaticClass()))
+		{
+			LockOnWidget = Cast<UWidgetComponent>(Component);
+			Hide();
+			break;
+		}
+	}
+}
+
+void ALockOn::Show()
+{
+	LockOnWidget->SetHiddenInGame(false);
+}
+
+void ALockOn::Hide()
+{
+	LockOnWidget->SetHiddenInGame(true);
+}
+
+void ALockOn::ToggleVisibility(const bool bVisibility)
+{
+	LockOnWidget->SetHiddenInGame(bVisibility);
 }
--- Source/MythologicalBoss/Public/Widgets/World/LockOn.h
+++ Source/MythologicalBoss/Public/Widgets/World/LockOn.h
@@ -14,6 +14,20 @@ class MYTHOLOGICALBOSS_API ALockOn final : public AActor
 public:	
 	ALockOn();
 
+	// Set lock-on widget to visible
+	UFUNCTION(BlueprintCallable, Category = "Lock-On")
+		void Show();
+	
+	// Set lock-on widget to hidden
+	UFUNCTION(BlueprintCallable, Category = "Lock-On")
+		void Hide();
+
+	// Toggle lock-on widget visiblility
+	UFUNCTION(BlueprintCallable, Category = "Lock-On")
+		void ToggleVisibility(bool bVisibility);
+
 protected:
 	void BeginPlay() override;
+
+	class UWidgetComponent* LockOnWidget;
 };
