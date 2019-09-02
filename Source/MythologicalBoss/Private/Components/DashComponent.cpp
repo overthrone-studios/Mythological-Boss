b35eeb5803b9aa428c39cf9290f07a3b23a7764e
Ali El Saleh <elsaleh78@gmail.com>
2019-09-03 00:16:14 +1000

    Implement the dash component

--- Source/MythologicalBoss/Private/Components/DashComponent.cpp
+++ Source/MythologicalBoss/Private/Components/DashComponent.cpp
@@ -2,17 +2,33 @@
 
 #include "DashComponent.h"
 
+#include "GameFramework/Actor.h"
+
+#include "TimerManager.h"
 
 UDashComponent::UDashComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
-
 }
 
 void UDashComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	Owner = GetOwner();
+}
+
+bool UDashComponent::IsCooldownActive()
+{
+	return Owner->GetWorldTimerManager().IsTimerActive(DashCooldownTimer);
+}
+
+float UDashComponent::GetCooldownTimeRemaining()
+{
+	return Owner->GetWorldTimerManager().GetTimerRemaining(DashCooldownTimer);
+}
+
+void UDashComponent::StartCooldown()
+{
+	Owner->GetWorldTimerManager().SetTimer(DashCooldownTimer, DashCooldown, false);
 }
