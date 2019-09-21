6d2e7852427ed56c32687ee273ee1459d3991f16
Ali El Saleh <elsaleh78@gmail.com>
2019-09-21 22:50:58 +1000

    Add a custom game state class

---
+++ Source/MythologicalBoss/Public/OverthroneGameState.h
@@ -0,0 +1,20 @@
+// Copyright Overthrone Studios 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "OverthroneGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHOLOGICALBOSS_API AOverthroneGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
