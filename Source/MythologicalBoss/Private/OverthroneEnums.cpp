8217730ee0c2c695578bef67e4ccfda460dcd7ba
Ali El Saleh <elsaleh78@gmail.com>
2019-09-21 18:07:17 +1000

    Create a header file for enums only

---
+++ Source/MythologicalBoss/Private/OverthroneEnums.cpp
@@ -0,0 +1,3 @@
+// Copyright Overthrone Studios 2019
+
+#include "OverthroneEnums.h"
---
+++ Source/MythologicalBoss/Public/OverthroneEnums.h
@@ -0,0 +1,81 @@
+// Copyright Overthrone Studios 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+
+UENUM()
+enum EBuildConfig
+{
+	DebugBuild,
+	DevelopmentBuild,
+	ShippingBuild
+};
+
+UENUM()
+enum EDashType_Mordath
+{
+	Dash_Forward,
+	Dash_Backward,
+	Dash_Left,
+	Dash_Right
+};
+
+UENUM(BlueprintType)
+enum EBossStage
+{
+	Stage_1,
+	Stage_2,
+	Stage_3
+};
+
+UENUM()
+enum EAttackType_Player
+{
+	Light,
+	Heavy,
+	Special,
+	None
+};
+
+UENUM()
+enum EAttackType_Mordath
+{
+	ShortAttack_1,
+	ShortAttack_2,
+	ShortAttack_3,
+	LongAttack_1,
+	LongAttack_2,
+	LongAttack_3,
+	SpecialAttack_1,
+	SpecialAttack_2,
+	SpecialAttack_3
+};
+
+UENUM()
+enum EAttackCounters_Mordath
+{
+	Parryable,
+	Blockable,
+	NoCounter
+};
+
+UENUM()
+enum EBossRanges
+{
+	SuperClose,
+	Close,
+	Mid,
+	Far
+};
+
+/**
+ * A header file containing all enums that Overthrone uses
+ */
+class MYTHOLOGICALBOSS_API OverthroneEnums
+{
+public:
+	OverthroneEnums() = default;
+	~OverthroneEnums() = default;
+};
