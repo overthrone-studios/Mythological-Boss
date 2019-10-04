98316b3513aafa393526b5f4932868ebf5a94de0
Ali El Saleh <elsaleh78@gmail.com>
2019-10-04 21:23:25 +1000

    Allow apply damage on low stamina

--- Source/MythologicalBoss/Private/AnimNotifyStates/AnimNotifyState_ApplyDamagePlayer.cpp
+++ Source/MythologicalBoss/Private/AnimNotifyStates/AnimNotifyState_ApplyDamagePlayer.cpp
@@ -52,7 +52,7 @@ void UAnimNotifyState_ApplyDamagePlayer::OnHit(USkeletalMeshComponent* MeshComp)
 
 	float Multiplier = 1.0f;
 
-	if (HitActor && HitActor->IsA(ACharacter::StaticClass()) && HitActor->bCanBeDamaged && !Ylva->IsLowStamina())
+	if (HitActor && HitActor->IsA(ACharacter::StaticClass()) && HitActor->bCanBeDamaged /*&& !Ylva->IsLowStamina()*/)
 	{
 		bIsHit = true;
 
