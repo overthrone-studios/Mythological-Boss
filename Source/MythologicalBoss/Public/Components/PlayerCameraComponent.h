0fec653bcd03ae3b9490074aede9558326ccc71a
Ali El Saleh <elsaleh78@gmail.com>
2019-10-16 20:51:49 +1100

    Create a custom camera component for the player

---
+++ Source/MythologicalBoss/Public/Components/PlayerCameraComponent.h
@@ -0,0 +1,20 @@
+// Copyright Overthrone Studios 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "PlayerCameraComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHOLOGICALBOSS_API UPlayerCameraComponent : public UCameraComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
