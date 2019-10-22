2441c63038e3b368f5c2fb28d57376724f707817
Ali El Saleh <elsaleh78@gmail.com>
2019-10-22 14:30:13 +1100

    Fix an issue where mordath would cancel his attack animation when Ylva is dead

--- Source/MythologicalBoss/Private/Boss/MordathBase.cpp
+++ Source/MythologicalBoss/Private/Boss/MordathBase.cpp
@@ -169,7 +169,7 @@ void AMordathBase::OnEnterAnyState(int32 ID, FName Name)
 
 void AMordathBase::UpdateAnyState(int32 ID, FName Name, float Uptime, int32 Frames)
 {
-	if (GameState->IsPlayerDead() && HasFinishedAction())
+	if (GameState->IsPlayerDead() && !AnimInstance->Montage_IsPlaying(nullptr))
 	{
 		FSM->Stop();
 	}
@@ -743,7 +743,7 @@ void AMordathBase::OnExecutionTimeExpired()
 
 void AMordathBase::OnPlayerDeath()
 {
-	BossAIController->StopMovement();
+	StopMovement();
 
 	FSM->RemoveAllStatesExceptActive();
 }
