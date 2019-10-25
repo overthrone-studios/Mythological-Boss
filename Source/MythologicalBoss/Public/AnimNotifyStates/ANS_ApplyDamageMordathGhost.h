287830ed8625e0ee61f07a3d66a2b53c8f9fd4ef
Ali <elsaleh78@gmail.com>
2019-10-25 14:50:07 +1100

    Create new anim notify state for mordath ghost

---
+++ Source/MythologicalBoss/Public/AnimNotifyStates/ANS_ApplyDamageMordathGhost.h
@@ -0,0 +1,20 @@
+// Copyright Overthrone Studios 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimNotifyStates/AnimNotifyState_ApplyDamageBase.h"
+#include "ANS_ApplyDamageMordathGhost.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHOLOGICALBOSS_API UANS_ApplyDamageMordathGhost : public UAnimNotifyState_ApplyDamageBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
