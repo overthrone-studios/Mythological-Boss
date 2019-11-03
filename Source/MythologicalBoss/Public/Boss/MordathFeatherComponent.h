088d6dc0cb612b34fb79b32fdc364a3d5b2bba8b
Ali El Saleh <elsaleh78@gmail.com>
2019-11-04 00:37:04 +1100

    Create mordath feather component

---
+++ Source/MythologicalBoss/Public/Boss/MordathFeatherComponent.h
@@ -0,0 +1,20 @@
+// Copyright Overthrone Studios 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "MordathFeatherComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHOLOGICALBOSS_API UMordathFeatherComponent : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
