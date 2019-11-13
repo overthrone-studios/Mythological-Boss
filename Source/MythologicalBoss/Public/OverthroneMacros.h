46de6e133ce8f44d7f3c2db51b50907ae26414e0
Ali El Saleh <elsaleh78@gmail.com>
2019-11-13 12:15:03 +1100

    Create overthrone macros library

---
+++ Source/MythologicalBoss/Public/OverthroneMacros.h
@@ -0,0 +1,17 @@
+// Copyright Overthrone Studios 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#define CAST(Object, Type) Cast<Type>(Object)
+
+/**
+ * A header file containing list of useful macros that Overthrone uses
+ */
+class MYTHOLOGICALBOSS_API OverthroneMacros
+{
+public:
+	OverthroneMacros() = default;
+	~OverthroneMacros() = default;
+};
